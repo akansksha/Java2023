a2979d9b952148dbccbde9b5c4a5c6956659b51f
Akanksha Singh <akanksha_s@PORTER.COM>
2023-02-01 23:49:30 +0530

    Area of Circle

---
+++ CircleArea.java
@@ -0,0 +1,14 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float r = 10.5f;
+		final float pie = 3.14f;
+		double area = pie * r *r;
+		System.out.println("Area of circle :"+ area);
+		System.out.println("Area of circle :" + String.format("%.2f",+ area));
+		
+	}
+
+}
