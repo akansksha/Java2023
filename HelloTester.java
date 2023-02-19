c92bfc8acc5226a2a27f1caa92fbb322ad066a44
Akanksha Singh <akanksha_s@PORTER.COM>
2023-02-19 12:20:30 +0530

    FirstTester

---
+++ HelloTester.java
@@ -0,0 +1,16 @@
+
+public class HelloTester {
+	public static void main(String args[]) {
+		System.out.println("Hello");
+		int a = 10;
+		System.out.println(a);
+		getvariable();
+		
+	}
+	public static void getvariable()
+	{
+		int b = 30;
+		System.out.println(b);
+	}
+
+}
